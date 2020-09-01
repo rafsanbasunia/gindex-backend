e75019bcc021d61ed1e257e15d335174e572b85c
Rafsan Hossen <rafsanhossainrafsan@gmail.com>
2020-09-01 21:27:21 +0600

    Revert "Revert "Revert "fix 26"""
    This reverts commit 25cde19f5783f766be0a9d22dddfc9570193f184.

--- routes/pending/index.js
+++ routes/pending/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const checkOrigin = require("../../plugins/checkOrigin");
-const jwtVerify = require('../../plugins/jwtVerify');
 
 //Model Imports
 const User = require("../../models/user");
@@ -9,11 +8,35 @@ const PendingUser = require("../../models/pendingUser");
 
 router.post('/users', function(req, res){
 	if(checkOrigin(req.headers.origin)){
-		if(jwtVerify(req.headers.token)){
-			User.findOne({ email: req.body.adminuseremail }, function(error, result){
-					if(result){
-						if(result.admin){
-							PendingUser.find({ post: "User" }, function(error, result){
+		User.findOne({ email: req.body.adminuseremail }, function(error, result){
+				if(result){
+					if(result.admin){
+						PendingUser.find({ post: "User" }, function(error, result){
+							if(result.length == 0){
+								res.status(200).send({ auth: false, registered: true, message: "No Pending Users" })
+							} else {
+								res.status(200).send({ auth: true, registered: true, users: result })
+							}
+						})
+					} else {
+						res.status(200).send({ auth: false, registered: true, message: "You are Unauthorized" })
+					}
+				} else {
+					res.status(200).send({ auth: false, registered: false, message: "BAD REQUEST" })
+				}
+			})
+	} else {
+		res.status(200).send({ auth: false, message: "UNAUTHORIZED" })
+	}
+});
+
+router.post('/admins', function(req, res){
+	if(checkOrigin(req.headers.origin)){
+		User.findOne({ email: req.body.adminuseremail }, function(error, result){
+				if(result){
+					if(result.admin){
+						if(result.superadmin){
+							PendingUser.find({ post: "Admin" }, function(error, result){
 								if(result.length == 0){
 									res.status(200).send({ auth: false, registered: true, message: "No Pending Users" })
 								} else {
@@ -24,44 +47,12 @@ router.post('/users', function(req, res){
 							res.status(200).send({ auth: false, registered: true, message: "You are Unauthorized" })
 						}
 					} else {
-						res.status(200).send({ auth: false, registered: false, message: "BAD REQUEST" })
+						res.status(200).send({ auth: false, registered: true, message: "You are Unauthorized" })
 					}
-				})
-		} else {
-			res.status(200).send({ auth: false, message: "Bearer Token Not Valid" })
-		}
-	} else {
-		res.status(200).send({ auth: false, message: "UNAUTHORIZED" })
-	}
-});
-
-router.post('/admins', function(req, res){
-	if(checkOrigin(req.headers.origin)){
-		if(jwtVerify(req.headers.token)){
-			User.findOne({ email: req.body.adminuseremail }, function(error, result){
-					if(result){
-						if(result.admin){
-							if(result.superadmin){
-								PendingUser.find({ post: "Admin" }, function(error, result){
-									if(result.length == 0){
-										res.status(200).send({ auth: false, registered: true, message: "No Pending Users" })
-									} else {
-										res.status(200).send({ auth: true, registered: true, users: result })
-									}
-								})
-							} else {
-								res.status(200).send({ auth: false, registered: true, message: "You are Unauthorized" })
-							}
-						} else {
-							res.status(200).send({ auth: false, registered: true, message: "You are Unauthorized" })
-						}
-					} else {
-						res.status(200).send({ auth: false, registered: false, message: "BAD REQUEST" })
-					}
-				})
-		} else {
-			res.status(200).send({ auth: false, message: "Bearer Token Not Valid" })
-		}
+				} else {
+					res.status(200).send({ auth: false, registered: false, message: "BAD REQUEST" })
+				}
+			})
 	} else {
 		res.status(200).send({ auth: false, message: "UNAUTHORIZED" })
 	}
@@ -69,31 +60,27 @@ router.post('/admins', function(req, res){
 
 router.post('/superadmins', function(req, res){
 	if(checkOrigin(req.headers.origin)){
-		if(jwtVerify(req.headers.token)){
-			User.findOne({ email: req.body.adminuseremail }, function(error, result){
-					if(result){
-						if(result.admin){
-							if(result.superadmin){
-								PendingUser.find({ post: "SuperAdmin" }, function(error, result){
-									if(result.length == 0){
-										res.status(200).send({ auth: false, registered: true, message: "No Pending Users" })
-									} else {
-										res.status(200).send({ auth: true, registered: true, users: result })
-									}
-								})
-							} else {
-								res.status(200).send({ auth: false, registered: true, message: "You are Unauthorized" })
-							}
+		User.findOne({ email: req.body.adminuseremail }, function(error, result){
+				if(result){
+					if(result.admin){
+						if(result.superadmin){
+							PendingUser.find({ post: "SuperAdmin" }, function(error, result){
+								if(result.length == 0){
+									res.status(200).send({ auth: false, registered: true, message: "No Pending Users" })
+								} else {
+									res.status(200).send({ auth: true, registered: true, users: result })
+								}
+							})
 						} else {
 							res.status(200).send({ auth: false, registered: true, message: "You are Unauthorized" })
 						}
 					} else {
-						res.status(200).send({ auth: false, registered: false, message: "BAD REQUEST" })
+						res.status(200).send({ auth: false, registered: true, message: "You are Unauthorized" })
 					}
-				})
-		} else {
-			res.status(200).send({ auth: false, message: "Bearer Token Not Valid" })
-		}
+				} else {
+					res.status(200).send({ auth: false, registered: false, message: "BAD REQUEST" })
+				}
+			})
 	} else {
 		res.status(200).send({ auth: false, message: "UNAUTHORIZED" })
 	}
